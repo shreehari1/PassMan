b37758ebfb32e15cc3a2a2e05b442ff6378f64ba
Shreehari Marathe <shreeharimarathe@Shreeharis-MacBook-Air.local>
2023-05-18 12:18:34 +0530

    add login signin and totp page

---
+++ app/website/auth.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, flash
+
+
+auth = Blueprint('auth', '__name__')
+
+@auth.route('/login', methods= ['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route('/totp')
+def totp():
+    return render_template("totp.html")
+
+@auth.route('/signup', methods= ['GET', 'POST'])
+def signup():
+    errorMessages = {
+        "field": "",
+        "message": ""
+    }
+    if request.method == "POST":
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            errorMessages["field"] = "email"
+            errorMessages["message"] = "Email must be greater in length"
+        elif len(name) < 4:
+            errorMessages["field"] = "name"
+            errorMessages["message"] = "Name must be greater in length"
+        elif password1 != password2:
+            errorMessages["field"] = "password2"
+            errorMessages["message"] = "Both password must be same"
+        elif len(password1) < 4:
+            errorMessages["field"] = "password1"
+            errorMessages["message"] = "Password must be greater in length"
+        else:
+            # add user to db
+            pass
+
+    return render_template("sign-up.html", errormsg=errorMessages)
+
+@auth.route('/logout')
+def logout():
+    return "<p> Logout page <p>"
